51f319558ad07dd1f85f74bd45486e17790b6102
Marcos Tullio Silva de Souza <marcostullio.s@gmail.com>
2026-06-23 15:22:38 -0300

    redeploy

--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -1,21 +1,29 @@
 import type { NextAuthConfig } from "next-auth";
 
 export const authConfig: NextAuthConfig = {
-  session: { strategy: "jwt" },
-  pages: { signIn: "/login" },
   providers: [],
+
+  pages: {
+    signIn: "/login",
+  },
+
+  session: {
+    strategy: "jwt",
+  },
+
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
-        token.id = (user as any).id;
-        token.cargo = (user as any).cargo;
+        (token as any).id = (user as any).id;
+        (token as any).cargo = (user as any).cargo;
       }
       return token;
     },
+
     async session({ session, token }) {
       if (session.user) {
-        (session.user as any).id = token.id;
-        (session.user as any).cargo = token.cargo;
+        (session.user as any).id = (token as any).id;
+        (session.user as any).cargo = (token as any).cargo;
       }
       return session;
     },
