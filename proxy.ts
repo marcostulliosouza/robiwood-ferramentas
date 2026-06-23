7432bec42ce8bdf63e243b3e290a9b9cc4eb0121
Marcos Tullio Silva de Souza <marcostullio.s@gmail.com>
2026-06-23 15:26:00 -0300

    fix middleware

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth";
 import { authConfig } from "@/lib/auth.config";
 
-export const { auth: middleware } = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  return;
+});
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
